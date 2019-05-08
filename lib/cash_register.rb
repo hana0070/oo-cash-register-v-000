009115c3842dab1490f83e26ba6ae295ad874f78
HANA LEANDER <hana0070@gmail.com>
2019-05-08 13:05:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,6 +17,14 @@ class CashRegister
     self.last_transaction = amount * quantity
   end
 
+  def add_item(title, price, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
+  end
+
   def apply_discount
     if discount != 0
       self.total = (total * ((100.0 - discount.to_f)/100)).to_i #(total * ((100.0 - discount.to_f)/100)).to_i
